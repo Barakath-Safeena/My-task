f85325e5f6570137d015f6088dfcfe85a74bde1c
Barakath safeena <116183738+Barakath-Safeena@users.noreply.github.com>
2023-07-23 19:52:41 +0530

    Add files via upload

---
+++ httml/reg.js
@@ -0,0 +1,8 @@
+var name=prompt("what is your name?");
+alert("hi "+name + ", Here you can start filling your registration form");
+console.log(15+5);
+var arr=[0,1,7,8];
+console.log(arr[2]+arr[3]);
+
+
+
